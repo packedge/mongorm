1e8a143568ac75ed8ed86b587100c3713a7bc89a
Ashley Clarke <me@ashleyclarke.me>
2015-02-19 21:18:35 +0100

    start of attribute getters and setters

--- src/Model.php
+++ src/Model.php
@@ -7,14 +7,30 @@ use ReflectionClass;
 abstract class Model
 {
     /**
+     * The collection associated with the model.
+     *
      * @var String
      */
     protected $collection;
 
     /**
+     * The models attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [];
+
+    /**
+     * The models attributes original state.
+     *
+     * @var array
+     */
+    protected $original = [];
+
+    /**
      * @return Builder
      */
-    protected function newBuilder()
+    public function newBuilder()
     {
         $builder = new Builder;
         $builder->setModel($this);
@@ -42,5 +58,42 @@ abstract class Model
         $instance = new static;
 
         return $instance->newBuilder()->first($columns);
+    }
+
+    public function getAttribute($key)
+    {
+        // TODO: check for mutator method
+
+        // TODO: handle dates
+
+        // TODO: handle casts/datatypes
+
+        if(array_key_exists($key, $this->attributes))
+        {
+            return $this->attributes[$key];
+        }
+
+        // TODO: will need to check for method existing for relationships
+    }
+
+    public function setAttribute($key, $value)
+    {
+        // TODO: check for mutator method
+
+        // TODO: handle dates
+
+        // TODO: handle casts/datatypes
+
+        $this->attributes[$key] = $value;
+    }
+
+    public function __get($key)
+    {
+        return $this->getAttribute($key);
+    }
+
+    public function __set($key, $value)
+    {
+        $this->setAttribute($key, $value);
     }
 }
