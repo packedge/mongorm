c0c080dcb06bd8b441796eb2f093e2479a2e3bec
Ashley Clarke <me@ashleyclarke.me>
2015-02-21 01:18:07 +0100

    add dummy tests so its not empty

--- tests/Model/MassAssignmentTest.php
+++ tests/Model/MassAssignmentTest.php
@@ -2,6 +2,10 @@
 
 class MassAssignmentTest extends \PHPUnit_Framework_TestCase
 {
-
+    /** @test */
+    public function it_works()
+    {
+        $this->assertTrue(true);
+    }
 }
  
--- tests/Model/ToArrayJsonTest.php
+++ tests/Model/ToArrayJsonTest.php
@@ -12,7 +12,13 @@ class ToArrayJsonTest extends \PHPUnit_Framework_TestCase
     {
         $this->model = new Pineapple;
     }
-    
+
+    /** @test */
+    public function it_works()
+    {
+        $this->assertTrue(true);
+    }
+
     /*
      * Handle dates
      * handle casts
